7facea126491a4c6dfcdef026e1c078865b8bcf7
Jefry <jefrydagucci@ahloo.com>
2017-03-20 11:23:22 +0700

    Initial Commit

---
+++ JDGChatModule/JDGChatModule.h
@@ -0,0 +1,19 @@
+//
+//  JDGChatModule.h
+//  JDGChatModule
+//
+//  Created by Jefry Da Gucci on 3/20/17.
+//  Copyright © 2017 jefrydagucci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JDGChatModule.
+FOUNDATION_EXPORT double JDGChatModuleVersionNumber;
+
+//! Project version string for JDGChatModule.
+FOUNDATION_EXPORT const unsigned char JDGChatModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JDGChatModule/PublicHeader.h>
+
+
